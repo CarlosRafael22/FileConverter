481b82283dfd153e5bf442bbc0aaabedf6acd5e6
CRafael22 <carlosrafael093@gmail.com>
2021-02-13 19:25:33 -0300

    feat(ContentArea): Add logic to show texts accordingly

--- src/components/ContentArea/index.tsx
+++ src/components/ContentArea/index.tsx
@@ -1,14 +1,41 @@
+import { useContext } from 'react'
+import ConverterContext from '../../context'
 import { ContentAreaStyle, ArtWork, TitleStyle, SupportText } from './styles'
-//import Image from 'next/image'
+import retrieveCurrentStatus from '../../utils/status'
 
 const ContentArea = () => {
+  const state = useContext(ConverterContext)
+
+  const [
+    shouldShowProgress,
+    shouldShowFileName,
+    shouldShowSupportText,
+  ] = retrieveCurrentStatus(state)
+  const fileName = 'Red_drone.shapr'
+
+  const titleText = shouldShowFileName
+    ? fileName
+    : 'Drop your .shapr file here, or browse'
+
+  const supportText = state.isChoosingFormat
+    ? 'Convert to'
+    : 'Supports: .STEP, .STL, .IGES'
+  console.log(
+    'STATE NO CONTENTAREA: ',
+    shouldShowProgress,
+    state.allowDownloadRequest
+  )
   return (
     <ContentAreaStyle>
       <ArtWork>
         <img src={'/shapr3d.jpg'} width={80} height={80} />
       </ArtWork>
-      <TitleStyle>Drop your .shapr file here, or browse</TitleStyle>
-      <SupportText>Supports: .STEP, .STL, .IGES</SupportText>
+      <TitleStyle>{titleText}</TitleStyle>
+      {shouldShowSupportText && (
+        <SupportText isChoosingFormat={state.isChoosingFormat}>
+          {supportText}
+        </SupportText>
+      )}
     </ContentAreaStyle>
   )
 }
