f7944ea82a8ec1b0c49a4324293e812a084babc3
CRafael22 <carlosrafael093@gmail.com>
2021-02-13 15:13:32 -0300

    feat(Button): Add onPress prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,12 @@ import { ButtonStyle, ButtonText } from './styles'
 export type ButtonProps = {
   text: string
   fullWidth: boolean
+  onPress: Function
 }
 
-const Button = ({ text, fullWidth = false }: ButtonProps) => {
+const Button = ({ text, fullWidth = false, onPress }: ButtonProps) => {
   return (
-    <ButtonStyle fullWidth={fullWidth}>
+    <ButtonStyle fullWidth={fullWidth} onClick={onPress}>
       <ButtonText>{text}</ButtonText>
     </ButtonStyle>
   )
