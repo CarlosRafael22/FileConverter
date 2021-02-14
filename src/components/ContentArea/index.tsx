8bcabbf5760b9677161e0319642b8119ae93be21
CRafael22 <carlosrafael093@gmail.com>
2021-02-14 07:51:38 -0300

    feat(ContentArea): Add logic to upload file on SupportText click

--- src/components/ContentArea/index.tsx
+++ src/components/ContentArea/index.tsx
@@ -1,10 +1,50 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
+import axios from 'axios'
 import ConverterContext from '../../context'
 import { ContentAreaStyle, ArtWork, TitleStyle, SupportText } from './styles'
 import retrieveCurrentStatus from '../../utils/status'
 
 const ContentArea = () => {
   const state = useContext(ConverterContext)
+  const fileRef = useRef<HTMLInputElement | null>(null)
+  const formRef = useRef<HTMLFormElement | null>(null)
+
+  const sendData = async (formData) => {
+    const config = {
+      headers: { 'content-type': 'multipart/form-data' },
+      onUploadProgress: (event) => {
+        console.log(
+          `Current progress:`,
+          Math.round((event.loaded * 100) / event.total)
+        )
+      },
+    }
+
+    const response = await axios.post('/api/server', formData, config)
+
+    console.log('response', response.data)
+  }
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const formData = new FormData()
+
+    Array.from(event.target.files).forEach((file) => {
+      formData.append(event.target.name, file)
+    })
+
+    sendData(formData)
+
+    // formRef.current?.reset()
+  }
+
+  const onClickHandler = () => {
+    console.log('CLICOU')
+    fileRef.current?.click()
+  }
 
   const [
     shouldShowProgress,
@@ -27,15 +67,27 @@ const ContentArea = () => {
   )
   return (
     <ContentAreaStyle>
-      <ArtWork>
-        <img src={'/shapr3d.jpg'} width={80} height={80} />
-      </ArtWork>
-      <TitleStyle>{titleText}</TitleStyle>
-      {shouldShowSupportText && (
-        <SupportText isChoosingFormat={state.isChoosingFormat}>
-          {supportText}
-        </SupportText>
-      )}
+      <form>
+        <ArtWork>
+          <img src={'/shapr3d.jpg'} width={80} height={80} />
+        </ArtWork>
+        <TitleStyle>{titleText}</TitleStyle>
+        {shouldShowSupportText && (
+          <SupportText
+            isChoosingFormat={state.isChoosingFormat}
+            onClick={onClickHandler}
+          >
+            {supportText}
+          </SupportText>
+        )}
+        <input
+          name="file"
+          onChange={onChangeHandler}
+          ref={fileRef}
+          type="file"
+          style={{ display: 'none' }}
+        />
+      </form>
     </ContentAreaStyle>
   )
 }
