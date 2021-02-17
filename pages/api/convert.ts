a5f980613a2c470d07adb0508a07dc2606046ad4
CRafael22 <carlosrafael093@gmail.com>
2021-02-17 10:17:57 -0300

    feat(api/convert): Create endpoint to convert request

---
+++ pages/api/convert.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import apiHandler from '../../backend/apiHandler'
+
+const apiRoute = apiHandler.get((req: NextApiRequest, res: NextApiResponse) => {
+  // mock conversion sending status of the progress
+  console.log('SERVER GET')
+  res.status(200).send({ success: true })
+})
+
+export default apiRoute
