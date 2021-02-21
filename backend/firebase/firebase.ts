1706ee82601f850710719c53efa116d438e7a6cb
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-21 12:12:32 -0300

    fix(firebase): Remove unecessary fields on initialize

--- backend/firebase/firebase.ts
+++ backend/firebase/firebase.ts
@@ -4,16 +4,9 @@ import admin from 'firebase-admin'
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      type: process.env.FIREBASE_ADMIN_type,
       projectId: process.env.FIREBASE_ADMIN_project_id,
-      privateKeyId: process.env.FIREBASE_ADMIN_private_key_id,
       privateKey: process.env.FIREBASE_ADMIN_private_key,
       clientEmail: process.env.FIREBASE_ADMIN_client_email,
-      clientId: process.env.FIREBASE_ADMIN_client_id,
-      authUri: process.env.FIREBASE_ADMIN_auth_uri,
-      tokenUri: process.env.FIREBASE_ADMIN_token_uri,
-      authProviderX509CertUrl: process.env.FIREBASE_ADMIN_auth_provider_x509_cert_url,
-      clientX509CertUrl: process.env.FIREBASE_ADMIN_client_x509_cert_url
     }),
     databaseURL: "https://shapr3d-c2bba-default-rtdb.europe-west1.firebasedatabase.app"
   });
