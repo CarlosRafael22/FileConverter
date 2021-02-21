f9f45779a5eb7774decf49de86ac871ccd142f45
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-02-21 10:22:31 -0300

    feat(convert): Handle mock conversion and database call

--- pages/api/convert.ts
+++ pages/api/convert.ts
@@ -1,10 +1,37 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import apiHandler from '../../backend/apiHandler'
+import { database } from '../../backend/firebase/firebase'
 
-const apiRoute = apiHandler.get((req: NextApiRequest, res: NextApiResponse) => {
+const apiRoute = apiHandler.get(async (req: NextApiRequest, res: NextApiResponse) => {
   // mock conversion sending status of the progress
-  console.log('SERVER GET: ', req)
+  const { query: { filename } } = req
+  console.log('SERVER GET: ')
+  const fileRef = database.ref(`/files`).child(`${filename}`)
+  const setProgressCallback = (progress: number) => fileRef.set({ conversion: progress })
+  mockConversion(setProgressCallback)
   res.status(200).send({ success: true })
 })
+
+type mockCallbackType = (progress: number) => void
+
+const mockConversion = (callback: mockCallbackType): void => {
+  let progress = 0
+  let progressArray = []
+  while (progress < 100) {
+    let incProgress = progress + Math.floor(Math.random() * 19) + 10
+    progress = incProgress > 100 ? 100 : incProgress
+    console.log(progress, incProgress)
+    progressArray.push(progress)
+  }
+
+  progressArray.forEach((progress, index) => {
+    // clearTimeout(time)
+    setTimeout(() => {
+      console.log('progress no timeout: ', progress)
+      // socket.emit('updateProgress', progress)
+      callback(progress)
+    }, 1000 * index)
+  })
+}
 
 export default apiRoute
